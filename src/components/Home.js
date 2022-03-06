f25b01a51a906a10d230d31f5e73aa25a17fe1ae
MarlonDSC <marlonsubuyu.2012@gmail.com>
2022-03-06 19:53:07 -0400

    Retrieves the name of the company but a bug persists

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import firebaseApp from "../credenciales";
 import { getAuth, signOut } from "firebase/auth";
@@ -11,7 +11,7 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 const auth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
 
-const Home = ({correoUsuario}) => {
+const Home = ({ correoUsuario }) => {
 
     console.log(correoUsuario);
 
@@ -23,15 +23,18 @@ const Home = ({correoUsuario}) => {
         { id: 3, descripcion: "Tarea falsa 3", url: "https://picsum.photos/420" },
     ];
 
-    async function buscarDocumentoOCrearDocumento (idDocumento){
+    const [user, setUser] = useState({})
+
+    async function buscarDocumentoOCrearDocumento(idDocumento) {
         const docRef = doc(firestore, `Users/${idDocumento}`);
         const consulta = await getDoc(docRef);
-        if(consulta.exists()){
+        if (consulta.exists()) {
             const infDoc = consulta.data();
+            setUser({ correoUsuario, ...consulta.data() })
             return infDoc.tareas;
         }
         else {
-            setDoc(docRef, {tareas: [...fakeData]});
+            setDoc(docRef, { tareas: [...fakeData] });
             const consulta = await getDoc(docRef);
             const infDoc = consulta.data();
             return infDoc.tareas;
@@ -40,27 +43,31 @@ const Home = ({correoUsuario}) => {
     }
 
     useEffect(() => {
-        async function  fetchTareas(){
+        async function fetchTareas() {
             const tareasFetchadas = await buscarDocumentoOCrearDocumento(correoUsuario);
             setArrayTareas(tareasFetchadas);
         }
         fetchTareas();
+
+        // const getUser = async () => {
+        //     const snap = await getDoc(doc(firestore, `Users/${correoUsuario}`))
+        // }
+        // getUser()
     }, [])
 
     return <Container>
         <h4>
-            Hola, sesión iniciada
+            Hola, sesión iniciada. Eres empleado de {user.empresa}
         </h4>
         <Button onClick={() => signOut(auth)}>
             Cerrar sesión
         </Button>
         <hr />
-        <AgregarTarea arrayTareas = {arrayTareas} setArrayTareas = {setArrayTareas} correoUsuario =  {correoUsuario}/>
+        <AgregarTarea arrayTareas={arrayTareas} setArrayTareas={setArrayTareas} correoUsuario={correoUsuario} />
         {
-            arrayTareas ? <ListadoTareas arrayTareas = {arrayTareas} setArrayTareas = {setArrayTareas} correoUsuario =  {correoUsuario} />
-            : null
+            arrayTareas ? <ListadoTareas arrayTareas={arrayTareas} setArrayTareas={setArrayTareas} correoUsuario={correoUsuario} />
+                : null
         }
-        
     </Container>
 }
 
